a2e823389705d241bbbbcd513198921cc5dd1635
Ali Hadwan <alihadwan@Alis-MacBook-Pro.local>
2025-03-21 03:23:16 -0700

    added Post Creation

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>{
-    res.send("hello, express");
+    res.render("index.ejs");
 });
 
+app.post("/submit", (req,res)=>{
+    const title = req.body.title;
+    const content = req.body.content;
+    console.log(title, content);
+    res.render("index.ejs", {title: title, content: content});
+});
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });

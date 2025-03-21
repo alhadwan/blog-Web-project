31f7c6a5fda10191838fb0bd503094efd8d5f273
Ali Hadwan <alihadwan@Alis-MacBook-Pro.local>
2025-03-21 06:00:52 -0700

    added footer/header/postcreation pages

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>{
     res.render("index.ejs");
+});
+app.get("/postCreations", (req, res) =>{
+    res.render("postCreations");
 });
 
 app.post("/submit", (req,res)=>{

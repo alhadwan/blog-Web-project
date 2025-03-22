689fbdbf6f0e8128e127e53f0b1ecf4bcf512730
Ali Hadwan <alihadwan@Alis-MacBook-Pro.local>
2025-03-21 18:35:47 -0700

    added PostCreation and JSON

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+//absolute Path to the JSON File
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const postsFile = path.join(__dirname, "data", "posts.json");
+
 
 const app = express();
 const port = 3000;
+//const publish = [];
+
 
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -11,18 +22,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>{
-    res.render("index.ejs");
+    // reads the content of the file and save it on data
+    const data = fs.readFileSync(postsFile, "utf-8");
+    //parses the JSON string into an array
+  const publish = JSON.parse(data);
+  res.render("index.ejs", { publish });
+    
 });
 app.get("/postCreations", (req, res) =>{
     res.render("postCreations");
 });
 
 app.post("/submit", (req,res)=>{
-    const title = req.body.title;
-    const content = req.body.content;
-    console.log(title, content);
-    res.render("index.ejs", {title: title, content: content});
-});
+   
+    const posts = {
+       title: req.body.title,
+       content: req.body.content
+   };
+   const data = fs.readFileSync(postsFile, "utf-8");
+   const publish = JSON.parse(data);
+
+    publish.push(posts);
+
+    // Save back to file
+    fs.writeFileSync(postsFile, JSON.stringify(publish, null, 2));
+       res.redirect("/");
+   });
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });

6005ba375cad0740ec4c1c49f7cb5240aef377c4
Ali Hadwan <alihadwan@Alis-MacBook-Pro.local>
2025-03-22 19:47:11 -0700

    added Post Editing

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) =>{
     // reads the content of the file and save it on data
-    const data = fs.readFileSync(postsFile, "utf-8");
+  const data = fs.readFileSync(postsFile, "utf-8");
     //parses the JSON string into an array
   const publish = JSON.parse(data);
   res.render("index.ejs", { publish });
@@ -36,6 +36,7 @@ app.get("/postCreations", (req, res) =>{
 app.post("/submit", (req,res)=>{
    
     const posts = {
+       id: Date.now(),
        title: req.body.title,
        content: req.body.content
    };
@@ -48,7 +49,38 @@ app.post("/submit", (req,res)=>{
     fs.writeFileSync(postsFile, JSON.stringify(publish, null, 2));
        res.redirect("/");
    });
+app.get("/edit/:id", (req,res) =>{
+    // get id from the URL
+    const id = Number(req.params.id); 
+    console.log(req.params.id);
+    // load posts
+    const dataPosts = JSON.parse(fs.readFileSync(postsFile, "utf-8")); 
+    // find the matching post
+    const postToEdit = dataPosts.find(post => post.id === id);
+    console.log(postToEdit); 
+    res.render("postEdit.ejs", {post: postToEdit});
+});
+app.post("/edit/:id", (req,res) =>{
+//1. Read posts.json ➝ get all posts
+const data = fs.readFileSync(postsFile, "utf-8");
+const publish = JSON.parse(data);
 
+//2. Find the post where post.id === id
+const id = Number(req.params.id); 
+const postToEdit = publish.find(post => post.id === id);
+
+//3.Update that post’s title & content with req.body.title and req.body.content 
+if (postToEdit) {
+    postToEdit.title = req.body.title;
+    postToEdit.content = req.body.content;
+  }
+//4.Write the entire updated array back to posts.json using fs.writeFileSync
+fs.writeFileSync(postsFile, JSON.stringify(publish, null, 2));
+
+//5.Redirect to /       
+res.redirect("/");
+
+});
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
